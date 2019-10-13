ff023eeff4e2e394e525a8a06ed38b60fdbe8029
HUANG HUI MIN <teru0511@gmail.com>
2019-10-13 17:21:59 +0900

    sample googleSearch

---
+++ selenide-practice/src/test/java/googleSearch.java
@@ -0,0 +1,27 @@
+
+import static com.codeborne.selenide.Selenide.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.remote.BrowserType;
+
+import com.codeborne.selenide.Configuration;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class googleSearch {
+
+    @BeforeClass
+    public static void beforeClass() {
+    	WebDriverManager.chromedriver().setup();
+        Configuration.browser = BrowserType.CHROME;
+        //Configuration.headless = true;
+    }
+
+    @Test
+    public void testGoogleSearch() {
+        open("http://google.com");
+        $("[name=q]").val("ChromeDriver").submit();
+    }
+
+}
