a5942acb00539fc26616ef2784dfbda5fa0f009b
HUANG HUI MIN <teru0511@gmail.com>
2019-10-16 01:09:23 +0900

    minkara parts make/model search

--- selenide-practice/src/test/java/MinkaraPartsSearchTest.java
+++ selenide-practice/src/test/java/MinkaraPartsSearchTest.java
@@ -1,3 +1,4 @@
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 import org.junit.Before;
@@ -5,7 +6,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.remote.BrowserType;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
 
@@ -26,6 +26,9 @@ public class MinkaraPartsSearchTest {
 		$("#top_navi_parts").click();
 	}
 
+	/**
+	 *
+	 */
 	@Test
 	public void keywordSearchTest() {
 		$("#_ctl0_CPH1_Searcher_txtKeyword").sendKeys("bridgestone");
@@ -33,7 +36,19 @@ public class MinkaraPartsSearchTest {
 
 		ElementsCollection results = $$(".item-common");
 		results.shouldHaveSize(20);
-		results.get(0).shouldHave(Condition.text("BRIDGESTONE"));
+		results.get(0).shouldHave(text("BRIDGESTONE"));
+	}
+
+	@Test
+	public void carSearchTest() {
+		$("#pr_search > dd:nth-child(2) > a.thickbox > div").click();
+		switchTo().frame("TB_iframeContent");
+		$("#ddlMaker").selectOption("トヨタ");
+		$("#ddlModel").selectOption("86");
+		$("body > div.model_searchbtn > a:nth-child(2) > img").click();
+		$("#chkModelGroup_3_10248").setSelected(true);
+		$("#Form1 > div.modelcode_footer > div.modelcode_searchbtn > p > a > img").click();
+		$(".content-title-20170809").shouldHave(text("パーツ・商品 - 86 ［ ZN6 ］"));
 	}
 
 }
