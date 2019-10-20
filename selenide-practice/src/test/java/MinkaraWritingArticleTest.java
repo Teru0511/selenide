29a7cbc591a5bf9560bc8cbd4936ef33c57c309c
HUANG HUI MIN <teru0511@gmail.com>
2019-10-20 23:08:48 +0900

    minkara write blog

--- selenide-practice/src/test/java/MinkaraWritingArticleTest.java
+++ selenide-practice/src/test/java/MinkaraWritingArticleTest.java
@@ -1,4 +1,7 @@
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
+
+import java.time.LocalDateTime;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -10,6 +13,7 @@ import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class MinkaraWritingArticleTest {
+	private String articleName = LocalDateTime.now().toString();
 
 	@BeforeClass
 	public static void beforeClass() {
@@ -31,7 +35,24 @@ public class MinkaraWritingArticleTest {
 	@Test
 	public void testWritingBlog() {
 		$("#mypage_navi_blog").click();
+		$("#_ctl0_CPH2_MainEdit2_Edit1_TextBox_Title").sendKeys("Blog" + articleName);
+		$("#_ctl0_CPH2_MainEdit2_Edit1_FileUpLoad_ImageOldSupport1_FileUpload1")
+			.sendKeys("/Users/teru/git/selenide-sample/selenide-practice/img/cat005.jpg");
+		$("#_ctl0_CPH2_MainEdit2_Edit1_TextBox_Body").sendKeys("明日もいい日でありますように");
+		$("#_ctl0_CPH2_MainEdit2_Edit1_TagEntry1_boxTag").sendKeys("異世界 アニメ");
+		$("#_ctl0_CPH2_MainEdit2_Edit1_ddlPublishScope").selectOption("みん友まで公開");
+		$("#_ctl0_CPH2_MainEdit2_Edit1_Button_Commit").click();
+		$(".Message01").shouldHave(text("ありがとうございます。投稿を受け付けました。"));
 
+		//記事が投稿したか否かを確認する
+		$("#_ctl0_CPH2_MainThanks_HL_Nav1").click();
+		$("h1.contentsTitle").shouldHave(text("Blog" + articleName));
+
+		//投稿を削除する
+		//$(withText("編集")).click();
+		//$("#_ctl0_CPH2_MainEdit2_Edit1_Button_Delete").click();
+		//confirm();
+		//$(".Message01").shouldHave(text("ありがとうございます。投稿を受け付けました。"));
 	}
 
 
