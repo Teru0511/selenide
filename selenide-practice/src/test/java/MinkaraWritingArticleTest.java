54c4396f87516e06d1b660c8d4f7e4ec367a0a8b
HUANG HUI MIN <teru0511@gmail.com>
2019-10-21 19:21:17 +0900

    minkara writing parts article

--- selenide-practice/src/test/java/MinkaraWritingArticleTest.java
+++ selenide-practice/src/test/java/MinkaraWritingArticleTest.java
@@ -1,4 +1,5 @@
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 import java.time.LocalDateTime;
@@ -9,6 +10,7 @@ import org.junit.Test;
 import org.openqa.selenium.remote.BrowserType;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -32,27 +34,70 @@ public class MinkaraWritingArticleTest {
 		$("#head_top_btn_mypage").click();
 	}
 
-	@Test
-	public void testWritingBlog() {
-		$("#mypage_navi_blog").click();
-		$("#_ctl0_CPH2_MainEdit2_Edit1_TextBox_Title").sendKeys("Blog" + articleName);
-		$("#_ctl0_CPH2_MainEdit2_Edit1_FileUpLoad_ImageOldSupport1_FileUpload1")
-			.sendKeys("/Users/teru/git/selenide-sample/selenide-practice/img/cat005.jpg");
-		$("#_ctl0_CPH2_MainEdit2_Edit1_TextBox_Body").sendKeys("明日もいい日でありますように");
-		$("#_ctl0_CPH2_MainEdit2_Edit1_TagEntry1_boxTag").sendKeys("異世界 アニメ");
-		$("#_ctl0_CPH2_MainEdit2_Edit1_ddlPublishScope").selectOption("みん友まで公開");
-		$("#_ctl0_CPH2_MainEdit2_Edit1_Button_Commit").click();
-		$(".Message01").shouldHave(text("ありがとうございます。投稿を受け付けました。"));
+	//@Test
+	//public void testWritingBlog() {
+		//$("#mypage_navi_blog").click();
+		//$("#_ctl0_CPH2_MainEdit2_Edit1_TextBox_Title").sendKeys("Blog" + articleName);
+		//$("#_ctl0_CPH2_MainEdit2_Edit1_FileUpLoad_ImageOldSupport1_FileUpload1")
+			//.sendKeys("/Users/teru/git/selenide-sample/selenide-practice/img/cat005.jpg");
+		//$("#_ctl0_CPH2_MainEdit2_Edit1_TextBox_Body").sendKeys("明日もいい日でありますように");
+		//$("#_ctl0_CPH2_MainEdit2_Edit1_TagEntry1_boxTag").sendKeys("異世界 アニメ");
+		//$("#_ctl0_CPH2_MainEdit2_Edit1_ddlPublishScope").selectOption("みん友まで公開");
+		//$("#_ctl0_CPH2_MainEdit2_Edit1_Button_Commit").click();
+		//$(".Message01").shouldHave(text("ありがとうございます。投稿を受け付けました。"));
 
-		//記事が投稿したか否かを確認する
-		$("#_ctl0_CPH2_MainThanks_HL_Nav1").click();
-		$("h1.contentsTitle").shouldHave(text("Blog" + articleName));
+		//投稿が正常に完了したのかどうかを確認する
+		//$("#_ctl0_CPH2_MainThanks_HL_Nav1").click();
+		//$("h1.contentsTitle").shouldHave(text("Blog" + articleName));
 
 		//投稿を削除する
 		//$(withText("編集")).click();
 		//$("#_ctl0_CPH2_MainEdit2_Edit1_Button_Delete").click();
 		//confirm();
 		//$(".Message01").shouldHave(text("ありがとうございます。投稿を受け付けました。"));
+	//}
+
+	@Test
+	public void testWritingParts() {
+		$("#mypage_navi_parts").click();
+		$("#_ctl0_CPH1_btnNewEditCarContents").click();
+		$("#mainImageArea > div > div > div > input.inputImage")
+			.sendKeys("/Users/teru/git/selenide-sample/selenide-practice/img/cat005.jpg");
+		$("#partsSelect").click();
+		$("div.category").click();
+		$("#categoryLargeCategoryList > li:nth-child(1)").click();
+		$("#categoryLargeCategoryList > li.large_category_item.active > ul > li:nth-child(2)").click();
+		$("#txtMakerSearch").sendKeys("bridgestone");
+		$$("#makerSearchResultList > li").find(text("BRIDGESTONE")).click();
+		$("#txtPartsSearch").sendKeys("re070");
+		$$("#partsSearchResultList > li").find(text("POTENZA RE070 225/40R18")).click();
+		$("#btnPartsFinish").click();
+		$("#mainText").sendKeys("パーツ投稿テスト");
+		$("#appendImageArea > div > div > div > input.inputImage")
+			.sendKeys("/Users/teru/git/selenide-sample/selenide-practice/img/cat006.jpg");
+		$("#purchaseRouteType").selectOption(2);
+		$("#purchaseRoute").selectOption(3);
+		$("#purchaseRouteMemo").sendKeys("割引10%");
+		$("#purchasePrice").sendKeys("900");
+		$("#regularPrice").sendKeys("1000");
+		$("#recommendAdd").click();
+		$(".clipped").click();
+		$("#recommendClipList > li:nth-child(2) > input").click();
+		$(".ui-widget-content.ui-autocomplete-input").sendKeys("Test ");
+		$("#post").click();
+		confirm();
+
+		//投稿が正常に完了したのかどうかを確認する
+		$(".Message01").shouldHave(text("ありがとうございます。投稿を受け付けました。"));
+	    String partsURL = WebDriverRunner.getWebDriver().getCurrentUrl();
+	    partsURL = partsURL.replace( "thanks.aspx?type=1", "parts.aspx");
+	    open(partsURL);
+	    $(".userCarPhotoMemo").shouldHave(text("パーツ投稿テスト"));
+
+	    //投稿を削除する
+	  	$(withText("編集")).click();
+	  	$("#delete").click();
+	  	confirm();
 	}
 
 
